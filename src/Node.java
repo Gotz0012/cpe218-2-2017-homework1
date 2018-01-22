bffd5158cc30c44159e9688324034f5b98d2782b
Gotz0012 <35621866+Gotz0012@users.noreply.github.com>
2018-01-23 04:30:48 +0700

    1.2

---
+++ src/Node.java
@@ -0,0 +1,16 @@
+public class Node {
+    Character key;
+    int value ;
+    Node left = null;
+    Node right = null;
+    Node parent = null;
+
+    public Node(char key)
+    {
+        this.key = key;
+
+    }
+    public Node(){
+    }
+
+}
